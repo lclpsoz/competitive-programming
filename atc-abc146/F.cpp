ea65ecf646c9936224ad27c32b53e066d3efd7ab
lclpsoz <lclpsoz@gmail.com>
2020-10-03 01:09:39 -0300

    add upsolving to AtCoder problem F from contest abc146.

--- atc-abc146/F.cpp
+++ atc-abc146/F.cpp
@@ -29,12 +29,51 @@ using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __g
 
 ////////////////////////// Solution starts below. //////////////////////////////
 
+const int N = 1e5 + 10;
 
+int n, m;
+int dp[N];
+string s;
 
 int main () {
-	// ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cout.precision(10);
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(10);
+
+	cin >> n >> m;
+	cin >> s;
+	deque<int> avail;
+	avail.push_back(n);
+	for(int i = n-1; i >= 0; i--) {
+		if(LEN(avail) and avail.back()-i > m) avail.pop_back();
+		if(s[i] == '0') {
+			if(LEN(avail)) {
+				dp[i] = dp[avail.back()]+1;
+				avail.push_front(i);
+			}
+			else
+				dp[i] = -1;
+		}
+		else
+			dp[i] = -1;
+	}
+	// for(int i = 0; i <= n; i++)
+	// 	cout << dp[i] << " \n"[i==n];
+	vector<int> ans;
+	int val_now = dp[0];
+	if(val_now == -1)
+		cout << "-1\n";
+	else {
+		for(int i = 0; i <= n; i++)
+			if(dp[i] == val_now) {
+				ans.push_back(i);
+				val_now--;
+			}
+		for(int i = 1; i < LEN(ans); i++) {
+			assert(ans[i]-ans[i-1] <= m);
+			cout << ans[i]-ans[i-1] << " \n"[i==LEN(ans)-1];
+		}
+	}
 
 	return 0;
 }

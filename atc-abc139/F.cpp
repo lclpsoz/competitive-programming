1076ede65649b100cf7bb865a9406c49f45c5105
lclpsoz <lclpsoz@gmail.com>
2020-10-18 05:43:19 -0300

    add upsolving for problem F from AtCoder contest abc139.

--- atc-abc139/F.cpp
+++ atc-abc139/F.cpp
@@ -37,32 +37,9 @@ ll cross(pll a, pll b) {
 
 int quarter(pll a) {
 	if(a.x >= 0 and a.y >= 0) return 1;
-	else if(a.x >= 0 and a.y < 0) return 2;
-	else if(a.x < 0 and a.y <= 0) return 3;
+	else if(a.x < 0 and a.y >= 0) return 2;
+	else if(a.x < 0 and a.y < 0) return 3;
 	else return 4;
-}
-
-vector<pll> ger(int n) {
-	mt19937_64 rng((llu)chrono::steady_clock::now().time_since_epoch().count());
-	uniform_int_distribution<ll> distInt(-3, 3);
-	vector<pll> ret;
-	while(n--)
-		ret.push_back({distInt(rng), distInt(rng)});
-	return ret;
-}
-
-ll brute(vector<pll> &vec) {
-	int n = LEN(vec);
-	ll ret = 0;
-	for(int i = 0; i < (1<<n); i++) {
-		pll now = {0, 0};
-		for(int j = 0; j < n; j++)
-			if((1<<j)&i)
-				now.x += vec[j].x, now.y += vec[j].y;
-		ret = max(ret, now.x*now.x + now.y*now.y);
-	}
-
-	return ret;
 }
 
 int main () {
@@ -70,23 +47,20 @@ int main () {
     cin.tie(NULL);
     cout.precision(30);
 
-
 	int n;
 	cin >> n;
 	vector<pll> vec;
 	for(int i = 0; i < n; i++) {
 		ll x, y;
 		cin >> x >> y;
-		vec.push_back({x, y});
+		if(x or y)
+			vec.push_back({x, y});
 	}
-	// uniform_int_distribution<ll> distInt(-1000000000, 1000000000);
-	// pll base = {distInt(rng), distInt(rng)};
+	n = LEN(vec);
 	sort(ALL(vec), [&](const pll &lhs, const pll &rhs) {
 		if(quarter(lhs) == quarter(rhs)) return cross(lhs, rhs) > 0;
 		else return quarter(lhs) < quarter(rhs);
 	});
-	// for(auto p : vec)
-	// 	cout << p.x << ' ' << p.y << '\n';
 
 	ll ans = 0;
 	for(int i = 0; i < n; i++) {
@@ -95,10 +69,10 @@ int main () {
 			int p = j%n;
 			now.x += vec[p].x, now.y += vec[p].y;
 			ans = max(ans, now.x*now.x + now.y*now.y);
-			// cout << i << ' '
 		}
 	}
-	cout << sqrtl((ld)ans) << '\n';
+	cout << sqrt((ld)ans) << '\n';
+
 
 	return 0;
 }

a650644ab758101123aa6847badadc444439a01b
lclpsoz <lclpsoz@gmail.com>
2020-11-19 20:16:06 -0300

    add solutions to CF contest 1452

---
+++ CF/1452/b/b.cpp
@@ -0,0 +1,133 @@
+#include "bits/stdc++.h"
+#include "ext/pb_ds/tree_policy.hpp"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+////////////// Prewritten code follows. Look down for solution. ////////////////
+#define x first
+#define y second
+#define LEN(x) ((int)(x).size())
+#define ALL(x) x.begin(), x.end()
+using ll = long long;
+using llu = long long unsigned;
+using ld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vpii = vector<pii>;
+
+template<typename T>
+const T INF = (is_same<T, int>::value ? 1e9 : 1e18);
+const ld EPS = 1e-9;
+const int MOD = 1;
+
+inline int fcmp(ld x, ld y = 0, ld tol = EPS) {
+	return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+
+inline int mod(ll x, int m = MOD) {
+	return (int)(((x%m) + m)%m);
+}
+
+template<typename T, typename M = __gnu_pbds::null_type>
+using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+////////////////////////// Solution starts below. //////////////////////////////
+
+bool valid(vi vec) {
+	// if(vec[0] == 1 and vec[1] == 1 and vec[2] == 2)
+		// cout << "HERE!\n";
+	for(int i = 0; i < LEN(vec); i++) {
+		int delta = 0;
+		int mx =-1;
+		for(int j = 0; j < LEN(vec); j++)
+			if(i!=j)
+				mx = max(vec[j], mx);
+		for(int j = 0; j < LEN(vec); j++)
+			if(i!=j)
+				delta += mx-vec[j];
+		// if(vec[0] == 1 and vec[1] == 1 and vec[2] == 2)
+		// 	cout << i << ' ' << delta << '\n';
+		if(delta > vec[i] or ((vec[i]-delta)%(LEN(vec)-1))) return false;
+	}
+	return true;
+}
+
+void brute() {
+	vector<int> vec = {0, 0, 0,0};
+	int mx = 9;
+	int p = 0;
+	set<vi> a;
+	while(1) {
+		if(valid(vec)) {
+			vi ax = vec;
+			sort(ALL(ax));
+			a.insert(ax);
+			// for( int v : vec)
+			// 	cout << v << ' ';
+			// cout << '\n';
+		}
+		p = 0;
+		vec[p]++;
+		while(p < LEN(vec)-1 and vec[p] > mx) {
+			vec[p] = 0;
+			vec[p+1]++;
+			++p;
+		}
+		if(p == LEN(vec)-1 and vec.back() > mx) break;
+	}
+	for(vi vv : a) {
+		for(int v : vv)
+			cout << v << ' ';
+		cout << '\n';
+	}
+
+	exit(0);
+}
+
+int mdc(int a, int b) {
+	if(a==0) return b;
+	return mdc(b%a, a);
+}
+
+int main () {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(10);
+
+	// brute();
+
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		vi vec;
+		ll sum = 0;
+		int mx = -1;
+		for(int i = 0; i < n; i++) {
+			int x;
+			cin >> x;
+			vec.push_back(x);
+			sum += x;
+			mx = max(mx, x);
+		}
+		// n--;
+		int ans = 0;
+		for(int i = mx;; i++) {
+			sum = sum - mx + i;
+			ans = i-mx;
+			int m = (i*1LL*(n-1))/mdc(i, n-1);
+			cout << "sum = " << sum << ", m = " << m << '\n';
+			if(sum%m == 0){
+				cout << ans << '\n';
+				break;
+			}
+			else if(((sum+m)/m)*m <= mx*n) {
+				cout << ans + (((sum+m)/m)*m)-sum << '\n';
+				break;
+			}
+		}
+
+	}
+
+	return 0;
+}
---
+++ CF/1452/d/d.cpp
@@ -0,0 +1,77 @@
+#include "bits/stdc++.h"
+#include "ext/pb_ds/tree_policy.hpp"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+////////////// Prewritten code follows. Look down for solution. ////////////////
+#define x first
+#define y second
+#define LEN(x) ((int)(x).size())
+#define ALL(x) x.begin(), x.end()
+using ll = long long;
+using llu = long long unsigned;
+using ld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vpii = vector<pii>;
+
+template<typename T>
+const T INF = (is_same<T, int>::value ? 1e9 : 1e18);
+const ld EPS = 1e-9;
+const int MOD = 998244353;
+
+inline int fcmp(ld x, ld y = 0, ld tol = EPS) {
+	return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+
+inline int mod(ll x, int m = MOD) {
+	return (int)(((x%m) + m)%m);
+}
+
+template<typename T, typename M = __gnu_pbds::null_type>
+using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+////////////////////////// Solution starts below. //////////////////////////////
+
+int n;
+
+int brute(int x) {
+	if(x < 0) return 0;
+	if(x == 0) return 1;
+	int ret = 0;
+	for(int i = 1; i <= x; i+=2)
+		ret += brute(x-i);
+	return ret;
+}
+
+int pow_mod(int b, int p) {
+	int ret = 1;
+	while(p) {
+		if(p&1) ret = (ret * 1LL * b)%MOD;
+		p>>=1;
+		b = (b*1LL*b)%MOD;
+	}
+	return ret;
+}
+
+int main () {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(10);
+
+	cin >> n;
+	int ans = 0;
+	// cout << brute(n) << '\n';
+
+	vi fib(n+10);
+	fib[0] = 0, fib[1] = 1;
+	for(int i = 2; i <= n; i++)
+		fib[i] = (fib[i-1]+fib[i-2])%MOD;
+	
+	// cout << pow_mod(2, n) << '\n';
+	int inv_mod = pow_mod(pow_mod(2, n), MOD-2);
+
+	// cout << fib[n] << ' ' << inv_mod << '\n';
+	cout << (fib[n]*1LL*inv_mod)%MOD << '\n';
+
+	return 0;
+}

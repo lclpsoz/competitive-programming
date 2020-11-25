455f1eb78de93d04c17f93039fc173e977794aa9
lclpsoz <lclpsoz@gmail.com>
2020-11-24 21:58:07 -0300

    add upsolving to CF problem 1374E2

--- CF/1374/e2/E2.cpp
+++ CF/1374/e2/E2.cpp
@@ -32,63 +32,123 @@ using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __g
 
 const int N = 2e5 + 10;
 
-int n, k;
-int both[N], a[N], b[N];
+int typ[N];
+
+void no() {
+	cout << "-1\n";
+	exit(0);
+}
+
+const int BOTH = 2;
+const int LFT = -1;
+const int RIG = 1;
+const int ZERO = 0;
+const int BOTH_LFT_RIG = -3;
 
 int main () {
-	// ios_base::sync_with_stdio(false);
-    // cin.tie(NULL);
-    // cout.precision(10);
-	scanf("%d %d", &n, &k);
-	vector<int> vec[3];
-	for(int i = 0; i < n; i++) {
-		int t, x, y;
-		scanf("%d %d %d", &t, &x, &y);
-		if(x == y and x == 1)
-			vec[0].push_back(t);
-		else if(x == 1 and y == 0)
-			vec[1].push_back(t);
-		else if(x == 0 and y == 1)
-			vec[2].push_back(t);
-	}
-	for(int i = 0; i < 3; i++) {
-		sort(vec[i].begin(), vec[i].end());
-		reverse(vec[i].begin(), vec[i].end());
-	}
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.precision(10);
 
-	ll ans = 0;
-	while(k) {
-		// printf("ka = %d, kb = %d\n", ka, kb);
-		if(ka and kb) {
-			if(LEN(vec[0]) and LEN(vec[1]) and LEN(vec[2]) and
-				vec[0].back() < vec[1].back()+vec[2].back()) {
-				ans += vec[0].back();
-				vec[0].pop_back();
-			} else if(LEN(vec[0]) and LEN(vec[1]) and LEN(vec[2]) and
-				vec[0].back() >= vec[1].back()+vec[2].back()) {
-				ans += vec[1].back();
-				ans += vec[2].back();
-				vec[1].pop_back();
-				vec[2].pop_back();
+	int n, m, k;
+	cin >> n >> m >> k;
+	int _v1, _v2;
+	_v1 = _v2 = 0;
+	vpii both, lft, rig, zero; // 2, -1, 1, 0
+	for(int i = 1; i <= n; i++) {
+		int v, x, y;
+		cin >> v >> x >> y;
+		_v1 += x, _v2 += y;
+		if(x and y) {
+			both.push_back({v, i});
+			typ[i] = BOTH;
+		}
+		else if(x)
+			lft.push_back({v, i}), typ[i] = LFT;
+		else if(y)
+			rig.push_back({v, i}), typ[i] = RIG;
+		else
+			zero.push_back({v, i}), typ[i] = ZERO;
+	}
+	sort(ALL(both));
+	sort(ALL(lft));
+	sort(ALL(rig));
+	sort(ALL(zero));
+	if(_v1 < k or _v2 < k)
+		no();
+	else {
+		int k_lft = 0, k_rig = 0;
+		int p_both = 0, p_lft = 0, p_rig = 0, p_zero = 0;
+		set<int> ans_idx;
+		ll ans_val = 0;
+		while(p_both < LEN(both) and k_lft < k) {
+			k_lft++;
+			ans_val += both[p_both].x;
+			ans_idx.insert(both[p_both++].y);
+		}
+		k_rig = k_lft;
+		while(p_lft < LEN(lft) and k_lft < k) {
+			k_lft++;
+			ans_val += lft[p_lft].x;
+			ans_idx.insert(lft[p_lft++].y);
+		}
+		while(p_rig < LEN(rig) and k_rig < k) {
+			k_rig++;
+			ans_val += rig[p_rig].x;
+			ans_idx.insert(rig[p_rig++].y);
+		}
 
-			}
-			else if(LEN(vec[0])) {
-				ans += vec[0].back();
-				vec[0].pop_back();
-			} else {
-				if(LEN(vec[1]) == 0 or LEN(vec[2]) == 0) {
-					printf("-1\n");
-					exit(0);
+		if(k_rig < k or k_lft < k or LEN(ans_idx) > m)
+			no();
+		else {
+			set<pii> opt;
+			while(LEN(ans_idx) < m) {
+				opt.clear();
+				if(p_both < LEN(both))
+					opt.insert(both[p_both]);
+				if(p_zero < LEN(zero))
+					opt.insert(zero[p_zero]);
+				if(p_lft < LEN(lft))
+					opt.insert(lft[p_lft]);
+				if(p_rig < LEN(rig))
+					opt.insert(rig[p_rig]);
+	
+				if(p_both and p_lft < LEN(lft) and p_rig < LEN(rig))
+					opt.insert({lft[p_lft].x+rig[p_rig].x-both[p_both-1].x, BOTH_LFT_RIG});
+
+				pii p = *opt.begin();
+				if(p.y >= 0) {
+					ans_idx.insert(p.y);
+					ans_val += p.x;
+					if(typ[p.y] == BOTH) {
+						p_both++;
+					}
+					else if(typ[p.y] == LFT) {
+						p_lft++;
+					}
+					else if(typ[p.y] == RIG) {
+						p_rig++;
+					}
+					else if(typ[p.y] == ZERO) {
+						p_zero++;
+					}
 				}
-				ans += vec[1].back();
-				ans += vec[2].back();
-				vec[1].pop_back();
-				vec[2].pop_back();
+				else if(p.y == BOTH_LFT_RIG) {
+					p_both--;
+					ans_idx.erase(both[p_both].y);
+					ans_val -= both[p_both].x;
+					ans_idx.insert(lft[p_lft].y);
+					ans_val += lft[p_lft++].x;
+					ans_idx.insert(rig[p_rig].y);
+					ans_val += rig[p_rig++].x;
+				}
 			}
-			ka--, kb--;
+			cout << ans_val << '\n';
+			for(int v : ans_idx)
+				cout << v << ' ';
+			cout << '\n';
 		}
 	}
-	printf("%lld\n", ans);
 
 	return 0;
 }

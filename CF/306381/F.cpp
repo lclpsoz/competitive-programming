0d0ea9c94ddba871ef38ca874461f2e14024e436
lclpsoz <lclpsoz@gmail.com>
2020-12-07 17:20:40 -0300

    add upsolving for CF 306381F

--- CF/306381/F.cpp
+++ CF/306381/F.cpp
@@ -32,24 +32,160 @@ using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __g
 
 ////////////////////////// Solution starts below. //////////////////////////////
 
+ll brute_ans (ll s1, ll s2, ll xl, ll xr) {
+	ll ans = 0;
+	// for (ll x = xl; x <= xr; x++)
+	// 	cout << bitset<4>(x) << '\n';
 
+	for (ll x = xl; x <= xr; x++) {
+		ll val = ((s1 * 1ll * x) ^ (s2 * 1ll * x));
+		// cout << x << ": " << bitset<5>(val) << '\n';
+		ll sum = val % MOD;
+		ans = (ans + sum) % MOD;
+		// cout << "  sum = " << sum << ", ans = " << ans << '\n';
+	}
+	return ans;
+}
+
+int dp[100][2];
+int target[100];
+
+ll solve (int pos, bool smaller, string &number) {
+	// for (int i = 0; i < 2*pos; i++) cout << ' ';
+	// cout << number << ": " << pos << ' ' << smaller << '\n';
+
+	if (pos == -1)
+		return 1LL;
+
+	ll ret;
+	// int &ret = dp[pos][smaller];
+	// if (ret != -1)
+	// 	return ret;
+
+	if (target[pos] == -1) {
+		if (smaller)
+			ret = 2*solve (pos-1, smaller, number);
+		else if (number[pos] == '1') {
+			ret = solve (pos-1, true, number) + 
+					solve (pos-1, false, number);
+		}
+		else {
+			ret = solve (pos-1, false, number);
+		}
+	}
+	else {
+		if (smaller)
+			ret = solve (pos-1, smaller, number);
+		else if (target[pos] == 1) {
+			if (number[pos] == '0')
+				ret = 0LL;
+			else
+				ret = solve (pos-1, smaller, number);
+		}
+		else {
+			if (number[pos] == '1')
+				ret = solve (pos-1, true, number);
+			else
+				ret = solve (pos-1, smaller, number);
+		}
+	}
+
+	return ret;
+}
+
+ll solve (string &number, int p1, int c1, int p2 = -1, int c2 = -1) {
+	target[p1] = c1;
+	if (p2 != -1)
+		target[p2] = c2;
+	ll ret = solve (LEN(number)-1, 0, number);
+	target[p1] = -1;
+	target[p2] = -1;
+	return ret;
+}
+
+string bin_string (ll val) {
+	if (val == 0) return "0";
+	memset(target, -1, sizeof target);
+	// cerr << val << ": " << bitset<70>(val) << endl;
+	string number = bitset<70>(val).to_string();
+	reverse(ALL(number));
+	while (number.back() == '0') number.pop_back();
+	reverse(ALL(number));
+	return number;
+}
 
 int main () {
 	freopen("geometry.in", "r", stdin);
 	// freopen("FILE_NAME_OUTPUT.EXTENSION", "w", stdout);
-	// ios_base::sync_with_stdio(false);
-	// cin.tie(NULL);
-	// cout.precision(10);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.precision(10);
+
+
+	memset(target, -1, sizeof target);
+
+	// string number = bin_string((1LLU<<62) - 1);
+	// cerr << solve(0, 0, number) << '\n';
+	// test_count_bit();
 
 	int t;
 	cin >> t;
+	map<ll, int> mp;
 	while (t--) {
-		long long s1, s2, xl, xr;
+		// cerr << "t = " << t << '\n';
+		ll s1, s2, xl, xr;
 		cin >> s1 >> s2 >> xl >> xr;
+		ll ans = 0;
+		if (s1 > s2) swap(s1, s2);
+		if (s1 < s2) {
+			// ll ans_brute = brute_ans (s1, s2, xl, xr);
+			// cout << "brute = " << ans_brute << '\n';
+			int k1 = __builtin_ctzll(s1);
+			int k2 = __builtin_ctzll(s2);
+			int SHIFT = k2-k1;
+			s1 = s1%MOD;
+			s2 = s2%MOD;
+			string number_xr = bin_string(xr);
+			string number_xl = bin_string(xl-1);
+			reverse(ALL(number_xl));
+			reverse(ALL(number_xr));
+			for (int i = 0; i < min(LEN(number_xr), SHIFT); i++) {
+				// cerr << "  i = " << i << '\n';
+				
+				ll val_r = solve(number_xr, i, 1);
+				ll val_l = 0;
+				if (xl > 1 and i < LEN(number_xl))
+					val_l = solve(number_xl, i, 1);
+				ll v1 = (val_r-val_l)%MOD;
+				ll v2 = (1LL<<i)%MOD;
+				ll mlt = (v1*v2)%MOD;
+				ans = (ans+(s1*mlt)%MOD)%MOD;
+			}
 
-		long long ans = 0;
-		for (long long x = xl; x <= xr; x++) {
-			ans = (ans + (((s1 * 1ll * x) ^ (s2 * 1ll * x)) % MOD)) % MOD;
+			// cout << "\n__ TWO __\n";
+			for (int i = 0; i < LEN(number_xr); i++) {
+				// cerr << "  i = " << i << '\n';
+				
+				ll val_r = solve(number_xr, i, 1, i+SHIFT, 0);
+				
+				ll val_l = 0;
+				if (xl > 1 and i < LEN(number_xl))
+					val_l = solve(number_xl, i, 1, i+SHIFT, 0);
+				ll pw = (1LL<<i)%MOD;
+
+				ll v1 = (val_r-val_l)%MOD;
+				ans = (ans + (s2*((v1*pw)%MOD))%MOD)%MOD;
+
+				if (i >= SHIFT) {		
+					val_r = solve(number_xr, i, 1, i-SHIFT, 0);
+					if (xl > 1 and i < LEN(number_xl))
+						val_l = solve(number_xl, i, 1, i-SHIFT, 0);
+					// cout << "  val_r = " << val_r << '\n';
+					v1 = (val_r-val_l)%MOD;
+					ans = (ans + (s1*((v1*pw)%MOD)))%MOD;
+				};
+			}
+			// assert(ans_brute == ans%MOD);
 		}
 		cout << ans << endl;
 	}

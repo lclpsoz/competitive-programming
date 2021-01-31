1efa5fc7add3754b4adc4fde3cae6ea4c964acac
lclpsoz <lclpsoz@gmail.com>
2021-01-31 02:46:59 -0300

    add solution to some CSES problems

---
+++ CSES/sort/2216.cpp
@@ -0,0 +1,62 @@
+#include "bits/stdc++.h"
+#include "ext/pb_ds/tree_policy.hpp"
+#include "ext/pb_ds/assoc_container.hpp"
+using namespace std;
+////////////// Prewritten code follows. Look down for solution. ////////////////
+#define x first
+#define y second
+#define LEN(x) ((int)(x).size())
+#define ALL(x) x.begin(), x.end()
+using ll = long long;
+using llu = long long unsigned;
+using ld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vpii = vector<pii>;
+
+template<typename T>
+const T INF = (is_same<T, int>::value ? 1e9 : 1e18);
+const ld EPS = 1e-9;
+const int MOD = 1;
+
+inline int fcmp (ld x, ld y = 0, ld tol = EPS) {
+	return (x <= y + tol) ? (x + tol < y) ? -1 : 0 : 1;
+}
+
+inline int mod (ll x, int m = MOD) {
+	int ret = (int)x%m;
+	if (ret < 0)
+		ret += m;
+	return ret;
+}
+
+template<typename T, typename M = __gnu_pbds::null_type>
+using ordered_set = __gnu_pbds::tree<T, M, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+////////////////////////// Solution starts below. //////////////////////////////
+
+const int MAXN = 2E5 + 10;
+
+int main () {
+	// freopen("FILE_NAME_INPUT.EXTENSION", "r", stdin);
+	// freopen("FILE_NAME_OUTPUT.EXTENSION", "w", stdout);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.precision(10);
+
+	int n;
+	cin >> n;
+	vi vec(n);
+	vi mp(MAXN);
+	
+	for (int i = 0; i < n; i++) {
+		cin >> vec[i];
+		mp[vec[i]] = i;
+	}
+	int ans = 1;
+	for (int i = 1; i < n; i++)
+		ans += mp[i] > mp[i+1];
+	cout << ans << '\n';
+
+	return 0;
+}
